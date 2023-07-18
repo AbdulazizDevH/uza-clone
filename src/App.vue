2b9d2db1d1e70bb6dc61ef7cd175dbc72ea838b6
AbdulazizDevH <mr.abdulaziz00791@gmal.com>
2023-07-18 11:03:48 +0500

    A copy of UZA news agency...

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-orange-300 hello">
+    Hello
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="css">
+  .hello {
+    @apply first-letter:text-slate-900 text-white
+  }
+</style>
